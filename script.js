c837dcf336e76953f704b0aa197a197699ca5f9c
Zeroniks <57189702+Zeroniks@users.noreply.github.com>
2020-11-09 22:01:44 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+ var message="Привет Диман!";
+    var neonbasecolor="#000000";
+    var neontextcolor="red";
+    var neontextcolor2="#ffffff";
+    var flashspeed=500;
+    var flashingletters=7;
+    var flashingletters2=6;
+    var flashpause=1000;
+    var n=0;
+    if (document.all||document.getElementById){
+        document.write('<font color="'+neonbasecolor+'" size=+2>')
+          for (m=0;m<message.length;m++)
+    	     document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
+           document.write('</font>')
+    }
+    else
+       document.write(message)
+    function crossref(number){
+       var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number)
+          return crossobj
+    }
+    function neon(){
+          if (n==0){
+    	       for (m=0;m<message.length;m++)
+    	        crossref(m).style.color=neonbasecolor
+    }
+       crossref(n).style.color=neontextcolor
+          if (n>flashingletters-1) crossref(n-flashingletters).style.color=neontextcolor2
+          if (n>(flashingletters+flashingletters2)-1) crossref(n-flashingletters-flashingletters2).style.color=neonbasecolor
+          if (n<message.length-1) n++
+       else{
+    	     n=0
+    	       clearInterval(flashing)
+          	setTimeout("beginneon()",flashpause)
+          	return
+    	}
+    }
+    function beginneon(){
+       if (document.all||document.getElementById)
+       flashing=setInterval("neon()",flashspeed)
+    }
+    beginneon();
